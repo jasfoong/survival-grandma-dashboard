84994002750944a94ea8387a375eed2d23b1d868
Crystina D <crystinadang@gmail.com>
2024-04-23 23:07:37 -0400

    Convert data to human related formats

--- scripts/weather-api.js
+++ scripts/weather-api.js
@@ -1,6 +1,9 @@
 // empty array set to pull data out of async function
 export let weatherData = [];
 
+const kelvinToCelcius = 273.15;
+const celcius = "°C";
+
 async function getWeather() {
     try {
         // latitude and longitude for the arctic - seperated for possible change of location
@@ -10,15 +13,37 @@ async function getWeather() {
         const apiKey = "877660ad03b7399dce36c1836b27e0b0";
         const response = await axios.get(baseUrl + apiKey);
         weatherData = response.data;
+
+        // find elements
+        const description = document.querySelector(".dash__description");
+        const temp = document.querySelector(".dash__temp");
+        const feelsLike = document.querySelector(".dash__feels-like");
+        const cloudy = document.querySelector(".dash__cloudy");
+        const sunrise = document.querySelector(".dash__sunrise");
+        const sunset = document.querySelector(".dash__sunset");
+        const weatherIcon = document.querySelector(".dash__icon");
+
+        // insert data
+        description.innerText = `The weather today will mainly be ${response.data.weather[0].description}`;
+        // temp in kelvins need to be converted and rounded
+        temp.innerText = Math.round(response.data.main.temp - kelvinToCelcius) + celcius;
+        feelsLike.innerText = Math.round(response.data.main.feels_like - kelvinToCelcius) + celcius;
+        cloudy.innerText = response.data.clouds.all;
+
+        // convert dates
+        function timeConverter(UNIX_timestamp){
+            const timestamp = new Date(UNIX_timestamp * 1000);
+            const hour = timestamp.getHours();
+            const min = timestamp.getMinutes();
+            const time = hour + ':' + min;
+            return time;
+          }
+        sunrise.innerText = timeConverter(response.data.sys.sunrise);
+        sunset.innerText = response.data.sys.sunset;
+        weatherIcon.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
     } catch (error) {
         console.error();
     }
 };
 
-// async functions must be run with an async function so it can be sequential
-async function storeWeatherData () {
-    await getWeather();
-    console.log(weatherData);
-};
-
-storeWeatherData ();
+getWeather();
