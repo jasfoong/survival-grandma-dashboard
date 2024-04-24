82c1bccbaeaf61e46b9e425668876dda051312ca
Crystina D <crystinadang@gmail.com>
2024-04-24 00:59:05 -0400

    Create lon and lat randomizer

--- scripts/weather-api.js
+++ scripts/weather-api.js
@@ -1,20 +1,45 @@
 // empty array set to pull data out of async function
 export let weatherData = [];
 
-const kelvinToCelcius = 273.15;
-const celcius = "°C";
+// temp in kelvins need to be converted and rounded
+function convertToCelcius(kelvin) {
+    const celcius = "°C";
+    const converted = Math.round(kelvin - 273.15) + celcius;
+    return converted; 
+};
 
+// convert time
+function timeConverter(UNIX_timestamp){
+    const timestamp = new Date(UNIX_timestamp * 1000);
+    const hour = timestamp.getHours();
+    const min = timestamp.getMinutes();
+    const time = hour + ':' + min;
+    return time;
+  }
+
+function getRandomInRange(from, to) {
+    return (Math.random() * (to - from) + from).toFixed(7) * 1;
+    // .toFixed() returns string, so ' * 1' is a trick to convert to number
+}
 export async function getWeather() {
     try {
         // latitude and longitude for the arctic - seperated for possible change of location
-        const lat = 76.2506;
-        const lon = 100.1140;
+        // const lat = 76.2506;
+        // const lon = 100.1140;
+
+        // latitude -90 to 90
+        const lat = getRandomInRange(-90, 90);
+        // longitude -180 to 180
+        const lon = getRandomInRange(-180, 180);
+
         const baseUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=`;
         const apiKey = "877660ad03b7399dce36c1836b27e0b0";
         const response = await axios.get(baseUrl + apiKey);
         weatherData = response.data;
+        console.log(weatherData);
 
         // find elements
+        const lastUpdate = document.querySelector(".dash__update");
         const description = document.querySelector(".dash__description");
         const temp = document.querySelector(".dash__temp");
         const feelsLike = document.querySelector(".dash__feels-like");
@@ -22,26 +47,21 @@ export async function getWeather() {
         const sunrise = document.querySelector(".dash__sunrise");
         const sunset = document.querySelector(".dash__sunset");
         const weatherIcon = document.querySelector(".dash__icon");
+        const mapLocation = document.querySelector(".dash__map");
 
         // insert data
+        lastUpdate.innerText = `This was last updated at ${Date().toLocaleString()}`;
         description.innerText = `The weather today will mainly be ${response.data.weather[0].description}`;
-        // temp in kelvins need to be converted and rounded
-        temp.innerText = Math.round(response.data.main.temp - kelvinToCelcius) + celcius;
-        feelsLike.innerText = Math.round(response.data.main.feels_like - kelvinToCelcius) + celcius;
-        cloudy.innerText = `The sky is covered with ${response.data.clouds.all}% clouds`;
 
-        // convert dates
-        function timeConverter(UNIX_timestamp){
-            const timestamp = new Date(UNIX_timestamp * 1000);
-            const hour = timestamp.getHours();
-            const min = timestamp.getMinutes();
-            const time = hour + ':' + min;
-            return time;
-          }
+        temp.innerText = convertToCelcius(response.data.main.temp);
+        feelsLike.innerText = convertToCelcius(response.data.main.feels_like);
+        cloudy.innerText = `The sky is covered with ${response.data.clouds.all}% clouds`;
 
         sunrise.innerText = timeConverter(response.data.sys.sunrise);
         sunset.innerText = timeConverter(response.data.sys.sunset);
         weatherIcon.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+
+        mapLocation.href = `https://www.google.com/maps/@${lat},${lon},15z?entry=ttu`;
     
     } catch (error) {
         console.error();
